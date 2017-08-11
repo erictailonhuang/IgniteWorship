1954eaae77d64384fec0317429011a89b0c310a5
Eric Huang <erichuang@Awesomesaus.local>
2017-08-11 01:15:55 -0700

    add mongo

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ portNo = int(os.environ.get("PORT", 5000))
 DB_NAME = "heroku_9trbkmfx"
 DB_HOST = "ds161262.mlab.com"
 DB_PORT = 61262
+DB_USER = "erichuang1234" 
+DB_PASS = "songcloud1234"
 
 '''for local'''
 # hostName = '0.0.0.0'
