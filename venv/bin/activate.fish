47c212c8c73f5785769b3a545cbdb27dea4ba10c
Eric Huang <erichuang@Awesomesaus.local>
2017-08-10 22:26:31 -0700

    initial

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/erichuang/Google Drive/Projects and Random Code/SongCloud/venv"
+set -gx VIRTUAL_ENV "/Users/erichuang/SongCloud/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
