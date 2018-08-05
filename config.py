e20e15a7145d244fbfa9e9d2b296a8ca81c938c6
Eric Huang <erichuang@Awesomesaus.local>
2018-08-05 01:02:18 -0700

    connectivity fix

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ portNo = int(os.environ.get("PORT", 5000))
 DB_NAME = "heroku_jzbkv8xb"
 DB_HOST = "ds213612.mlab.com"
 DB_PORT = 13612
-DB_USER = "huangeric" 
-DB_PASS = "IgniteWorship1234"
+DB_USER = "heroku_jzbkv8xb" 
+DB_PASS = "5l1t80k64m5tmta14o8q7dkqo6"
 
 '''for local'''
 # hostName = 'localhost'
